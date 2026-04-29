83d369716732644795b538c8a4b5fb93a381ee51
박형빈 <cavin1999@gmail.com>
2026-04-29 12:24:55 +0900

    fix : bool 상태확인 오기입 수정

--- SampleProject1/Mercenary.h
+++ SampleProject1/Mercenary.h
@@ -17,6 +17,6 @@ public:
     ~Mercenary();
     
     int Attack() const {return attackDamage;}
-    bool IsOwnerAlive(){return owner.expired();}//주인이 살아있는 지 확인
+    bool IsOwnerAlive(){return !owner.expired();}//주인이 살아있는 지 확인
     
 };
